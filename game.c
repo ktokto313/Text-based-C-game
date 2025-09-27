f73cd1e5f5381ee69ca7b3940727457d1b3dd521
ngocdai0411-dot <ngocdai0411@gmail.com>
2025-09-27 21:58:01 +0700

    Update game.c

--- game.c
+++ game.c
@@ -37,31 +37,35 @@ void doGameTick(Game * game){
 					printf("Lua chon khong hop le.\n");
 			}
 		} else {
-			printf("\n====MAIN MENU (Day %d) ===\n", day);
-		    printf("1. Kham pha\n");
-	        printf("2. Di chuyen\n");
-	        printf("3. Bien ve\n");
-	        printf("4. Thoat\n");
-	        printf("Chon: ");
-			scanf("%d", &choice);
-			
-			switch(choice){
-				case 1:
-					explore();
-					break;
-			    case 2:
-			    	move();
-			    	break;
-			    case 3:
-			    	go_back();
-			    	break;
-			    case 4:
-			    	return;
-				default:
-					printf("Lua chon khong hop le.\n");
-			}
-	        
-		}
+		 printf("\n=== LUA CHON HANH DONG ===\n");
+        printf("1. Kham pha\n");
+        printf("2. Di chuyen\n");
+        printf("3. Bien ve\n");
+        printf("4. Menu\n");
+        printf("5. Thoat\n");
+        printf("Nhap lua chon: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("-> Luong kham pha\n");
+                break;
+            case 2:
+                printf("-> Luong di chuyen\n");
+                break;
+            case 3:
+                printf("-> +1 buoi + luong di chuyen\n");
+                break;
+            case 4:
+                printf("-> Luong main menu\n");
+                break;
+            case 5:
+                printf("Thoat chuong trinh.\n");
+                return 0;
+            default:
+                printf("Lua chon khong hop le, vui long nhap lai.\n");
+        }
+    }
 	}
 	return 0;
 }
